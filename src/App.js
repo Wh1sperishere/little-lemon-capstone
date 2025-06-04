49e386c54e536a159df14f6462c84f1b2c200da6
Mohammad <mohammadbikoul@gmail.com>
2025-06-04 18:53:25 +0400

    dont forget to setup the homepage

--- src/App.js
+++ src/App.js
@@ -18,7 +18,7 @@ function App() {
         <nav className='navigation'>
           <ul>
             <li><Link to='/'>Home</Link></li>
-            <li><Link to='/Pages/About'>About</Link></li>
+            <li><Link to='/About'>About</Link></li>
             <li><Link to='/menu'>Menu</Link></li>
             <li><Link to='/reservations'>Reservations</Link></li>
             <li><Link to='/order'>Order Online</Link></li>
--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,3 +1,10 @@
-export default function Home() {
+function cards() {
+
+}
+
+
+function Home() {
     return <h1>I am THE Home</h1>
 }
+
+export default Home;
