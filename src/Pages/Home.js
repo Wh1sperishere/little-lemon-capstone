03d5759e959bce2abe1a8394472d4b3359b38dda
Mohammad <mohammadbikoul@gmail.com>
2025-06-05 12:09:30 +0400

    Updated the home page

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,5 +1,22 @@
-function cards() {
+function Hero() {
+    return (
+        <div className="heroSection">
+            <h1>Little Lemon</h1>
+            <h3>Chicago</h3>
+            <p>Idk</p>
+        </div>
+    );
+}
 
+
+
+
+
+function Cards() {
+    return (
+        <div className="food">
+        </div>
+    );
 }
 
 
