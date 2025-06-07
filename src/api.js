6c559d86351c5126e7dac6bb16a4b0c95ef2ba05
Mohammad <mohammadbikoul@gmail.com>
2025-06-07 13:44:48 +0400

    finally finished

--- src/api.js
+++
@@ -1,9 +0,0 @@
-export function fetchAPI(date) {
-  // Just for example; replace this with actual logic
-  return ["17:00", "18:00", "19:00", "20:00", "21:00"];
-}
-
-export function submitAPI(formData) {
-  console.log("Submitting data:", formData);
-  return true;
-}
